0ef53a986c4651f3bc5fe0241fa88b567245f73c
James911015 <32137620+James911015@users.noreply.github.com>
2025-04-16 11:23:41 -0500

    S3: delete settings.gradle custumer and laucher the project

---
+++ Seccion3/paymentchainparent - copia/domain/customer/src/main/java/com/paymentchain/customer/controllers/CustomerController.java
@@ -0,0 +1,64 @@
+package com.paymentchain.customer.controllers;
+
+import com.paymentchain.customer.entities.Customer;
+import com.paymentchain.customer.repositories.CustomerRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @Autowired
+    CustomerRepository repository;
+
+    @GetMapping()
+    public List<Customer> getAllCustomers() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCustomerById(@PathVariable Long id) {
+        Optional<Customer> customer = repository.findById(id);
+        if (customer.isPresent()) {
+            return new ResponseEntity<>(customer.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateCustomerById(@PathVariable Long id, @RequestBody Customer body) {
+        Optional<Customer> customer = repository.findById(id);
+        if (customer.isPresent()) {
+            Customer newCustomer = customer.get();
+            newCustomer.setName(body.getName());
+            newCustomer.setPhone(body.getPhone());
+            Customer update = repository.save(newCustomer);
+            return new ResponseEntity<>(update, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Customer> saveCustomer(@RequestBody Customer body) {
+        Customer save = new Customer();
+        save = body;
+        Customer saved = repository.save(save);
+        return ResponseEntity.ok(saved);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable Long id) {
+        repository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+}
